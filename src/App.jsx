f335efef0238ad0d883dfd592c11e0fa00005a53
Mary-No <mascha.novik@gmail.com>
2024-10-04 19:52:23 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <div className="Search">
+          <input type="search" id="site-search" name="q"/>
+          <button>Search</button>
+        </div>
+
+      </div>
+  );
+}
+
+export default App;
